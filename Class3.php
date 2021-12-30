acb0fbd9c2c028f8b33b5e5db55f4719ded0ea2d
Timo Michna <timomichna@yahoo.de>
2021-12-30 07:13:43 +0100

    Do stuff

--- Class3.php
+++ Class3.php
@@ -4,4 +4,20 @@ namespace Example\Monorepo1\NS\SubPackage3;
 
 class Class3 {
     public string $foo;
+
+    /**
+     * @return string
+     */
+    public function getFoo(): string
+    {
+        return $this->foo;
+    }
+
+    /**
+     * @param string $foo
+     */
+    public function setFoo(string $foo): void
+    {
+        $this->foo = $foo;
+    }
 }
